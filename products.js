20de745535726122ed917a32d7e6239de90c17da
אסף גרינברג <asafg999@gmail.com>
2025-06-19 10:42:54 +0300

    תקון נתיבי תמונות והוספת אימוגים לכפתורים

--- products.js
+++ products.js
@@ -4,7 +4,7 @@ const products = [
     category: ['home'],
     text: 'IRobot - איירובוט',
     link: 'https://s.click.aliexpress.com/e/_oCGw960',
-    image: '/assets/images/IROBOT.jpg',
+    image: 'assets/images/IROBOT.jpg',
     alt: 'IRobot',
   },
   {
@@ -12,7 +12,7 @@ const products = [
     category: ['kids'],
     text: 'מכונית פעלולים עם שלט רחוק ועם יכולת טיפוס',
     link: 'https://s.click.aliexpress.com/e/_opjxc80',
-    image: '/assets/images/PAALOLIM.jpg',
+    image: 'assets/images/PAALOLIM.jpg',
     alt: 'מכונית פעלולים',
   },
   {
@@ -20,7 +20,7 @@ const products = [
     category: ['home', 'bedroom'],
     text: 'הורג יתושים חשמלי',
     link: 'https://s.click.aliexpress.com/e/_omabbPE',
-    image: '/assets/images/OREGYATUSHIM.jpg',
+    image: 'assets/images/OREGYATUSHIM.jpg',
     alt: 'הורג יתושים חשמלי',
   },
   {
@@ -28,7 +28,7 @@ const products = [
     category: ['home', 'car'],
     text: 'שואב אבק לשימוש ביתי ולרכב',
     link: 'https://s.click.aliexpress.com/e/_ool0x9e',
-    image: '/assets/images/CAR_VACUUM.png',
+    image: 'assets/images/CAR_VACUUM.png',
     alt: 'שואב אבק',
   },
   {
@@ -36,7 +36,7 @@ const products = [
     category: ['home'],
     text: 'פח על גלגלים',
     link: 'https://s.click.aliexpress.com/e/_okljX88',
-    image: '/assets/images/TRASH.jpg',
+    image: 'assets/images/TRASH.jpg',
     alt: 'פח על גלגלים',
   },
   // הוסף כאן מוצרים נוספים...
