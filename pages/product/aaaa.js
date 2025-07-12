94692f99c21f9fd12188577bead52e8987c70b0c
AsafGrinberg <131565101+AsafGrinberg@users.noreply.github.com>
2025-07-12 20:21:13 +0300

    DELET aaaa.js

--- pages/product/aaaa.js
+++ pages/product/aaaa.js
@@ -1,13 +1,1 @@
-import '../styles/globals.css';
-import Header from '../components/Header';
-import '../styles/product.module.css';
 
-
-export default function App({ Component, pageProps }) {
-  return (
-    <>
-      <Header />
-      <Component {...pageProps} />
-    </> 
-  );
-}
